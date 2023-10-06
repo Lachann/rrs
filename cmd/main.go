14c13f96ae79021fdad5267470e7f09901ae4955
Luke Goral <luke.oidhche@gmail.com>
2023-10-06 16:32:32 +0200

    wip

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/Lachann/rrs/pkg/server"
 )
@@ -18,6 +19,18 @@ func main() {
 	host := os.Getenv("RRS_HOST")
 	if host == "" {
 		panic("RRS_HOST not set")
+	}
+
+	rickrollChance := os.Getenv("RRS_RICKROLL_CHANCE")
+	if rickrollChance == "" {
+		panic("RRS_RICKROLL_CHANCE not set")
+	}
+	chance, err := strconv.Atoi(rickrollChance)
+	if err != nil {
+		panic("RRS_RICKROLL_CHANCE must be an integer")
+	}
+	if chance < 30 || chance > 70 {
+		panic("RRS_RICKROLL_CHANCE must be between 30 and 70")
 	}
 
 	if err := server.Start(host, defaultRickroll, defaultPreviews); err != nil {
