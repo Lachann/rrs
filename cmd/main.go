51dd1c510e130dcec86c3a8dea52cd108bcf70b4
Luke Goral <luke.oidhche@gmail.com>
2025-08-07 23:01:52 +0200

    tweaks

--- cmd/main.go
+++ cmd/main.go
@@ -18,10 +18,10 @@ const defaultHost = ":8080"
 var useBackup = os.Getenv("RRS_USE_BACKUP_URL")
 
 // const defaultRickroll = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-// const defaultRickroll = "https://www.youtube.com/watch?v=tae8F4gkfiw"
+const defaultRickroll = "https://www.youtube.com/watch?v=tae8F4gkfiw"
 // const defaultRickroll = "https://www.youtube.com/watch?v=LpNVf8sczqU"
 // const defaultRickroll = "https://www.youtube.com/watch?v=oEtmFgonY3E"
-const defaultRickroll = "https://www.youtube.com/watch?v=zaPl-J_RvAo"
+// const defaultRickroll = "https://www.youtube.com/watch?v=zaPl-J_RvAo"
 const backupRickroll = "https://www.youtube.com/watch?v=knOXppaqBYY"
 
 func main() {
