c1a321d6a819351377f88c59f63ddf289bd40f0c
Luke Goral <luke.oidhche@gmail.com>
2026-02-24 00:52:07 +0100

    test 2

--- cmd/main.go
+++ cmd/main.go
@@ -21,8 +21,9 @@ var useBackup = os.Getenv("RRS_USE_BACKUP_URL")
 // const defaultRickroll = "https://www.youtube.com/watch?v=tae8F4gkfiw"
 // const defaultRickroll = "https://www.youtube.com/watch?v=LpNVf8sczqU"
 // const defaultRickroll = "https://www.youtube.com/watch?v=oEtmFgonY3E"
-// const defaultRickroll = "https://www.youtube.com/watch?v=zaPl-J_RvAo"
-const defaultRickroll = "https://www.youtube.com/watch?v=bTAlomrlDvo"
+const defaultRickroll = "https://www.youtube.com/watch?v=zaPl-J_RvAo"
+
+// const defaultRickroll = "https://www.youtube.com/watch?v=bTAlomrlDvo"
 
 // const defaultRickroll = "https://www.youtube.com/watch?v=PslQESlD4xs"
 // const defaultRickroll = "https://www.youtube.com/watch?v=6-KAnUQlR38"
